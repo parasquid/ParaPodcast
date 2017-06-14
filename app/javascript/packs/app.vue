15d7ed89ecac0a5f07a8a1c0b62dbe96413fed1d
parasquid <tristan.gomez@gmail.com>
2017-06-14 23:01:01 +0800

    add component to show the audio tag

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <audio controls>
+      <source src="http://rss.art19.com/episodes/e762b9e8-69b7-4951-a5cf-9f707251615f.mp3" type="audio/mpeg">
+      Your browser does not support the audio tag.
+    </audio>
   </div>
 </template>
 
